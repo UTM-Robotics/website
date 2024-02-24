6e30ff8d5f8d779a9eb658a60218e0c0308e923d
infultMK2 <infernusultimo@gmail.com>
2024-02-23 21:25:40 -0500

    Media viewport accessibility changes

--- src/components/details/eventDetails.js
+++ src/components/details/eventDetails.js
@@ -6,7 +6,7 @@ import sample4 from '../../assets/sample4.jpeg';
 const eventDetails = [
 	{
 		id: 1,
-		name: "Battle Bots",
+		name: "General Meeting: Exploring the world of Battle Bots!",
 		image: sample1,
 		tag: "IN PERSON",
 		description: "This is a battle bot. They battle. They bot.",
@@ -17,7 +17,7 @@ const eventDetails = [
 	},
 	{
 		id: 2,
-		name: "Arduino Workshop",
+		name: "The Floor is Lava! Beginner Arduino Workshop",
 		image: sample2,
 		tag: "IN PERSON",
 		description: "The floor is not lava, but you can change that!",
@@ -28,7 +28,7 @@ const eventDetails = [
 	},
 	{
 		id: 3,
-		name: "Robot Dog Accesibility!",
+		name: "Speaker Series: Robot Guide Dog!",
 		image: sample3,
 		tag: "HYBRID",
 		description: "They literally got that DAWG in them fr.",
@@ -39,7 +39,7 @@ const eventDetails = [
 	},
 	{
 		id: 4,
-		name: "Nuclear Reactor",
+		name: "Nuclear Reactor Series: A series where CRIME.",
 		image: sample4,
 		tag: "IN PERSON",
 		description: "Come with us to make a full hydrogen atomic bomb in 17 minutes! Ask PETA. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
