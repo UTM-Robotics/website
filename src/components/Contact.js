83b8ad1101b9d1d91a42b55fb163717460694356
alexapostolu <apostolu240@gmail.com>
2024-02-28 10:31:31 -0500

    email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,95 +1,101 @@
 import React, { useState, useEffect } from 'react';
+import emailjs from 'emailjs-com';
 
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
 
 import '../styles/Contact.css';
 
 const Contact = () => {
-	useEffect(() => {
-		const aboutContainer = document.querySelector('.contactContainer');
-		aboutContainer.classList.add('fade-in-up');
-	}, []);
+  useEffect(() => {
+    const aboutContainer = document.querySelector('.contactContainer');
+    aboutContainer.classList.add('fade-in-up');
+  }, []);
 
-	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		message: ''
-	});
-	
-	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
-	
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    subject: '',
+    message: ''
+  });
 
-		// Handle form submission here. We probably need like node.js and express
-		//
-		//
+  const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      e.target,
+      process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+    ).then((result) => {
+      setFormData({
+        email: '',
+        name: '',
+        subject: '',
+        message: ''
+      });
+      console.log(result.text);
+    }).catch((error) => {
+      console.log(error.text);
+    });
+  };
 
+  return (
+    <div>
+      <Header />
 
-		// Clear the form after submission.
-		setFormData({ name: '', email: '', message: '' });
-	};
-	
-	return (
-		<div>
-			<Header/>
+      <div className='contactContainer'>
+        <h2 id='title'>Email us directly, or contact us using the form anonymously!</h2>
 
-			<div className='contactContainer'>
-				<h2 id='title'>Email us directly, or contact us using the form anonymously!</h2>
+        <div className='info'>
+          <div className='contactInfo'>
+            <h2>Email</h2>
+            <p>robotics@utmsu.ca</p>
 
-				<div className='info'>
-					<div className='contactInfo'>
+            <h2>Address</h2>
+            <p>???</p>
+          </div>
 
-						<h2>Email</h2>
-						<p>robotics@utmsu.ca</p>
+          <form className='contactForm' onSubmit={sendEmail}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </label>
 
-						<h2>Address</h2>
-						<p>???</p>
-					</div>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
 
-					<form className='contactForm' onSubmit={handleSubmit}>
-						<label>
-							Name:
-							<input
-							type="text"
-							name="name"
-							value={formData.name}
-							onChange={handleChange}
-							/>
-						</label>
+            <label>
+              Message:
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
 
-						<label>
-							Email:
-							<input
-							type="email"
-							name="email"
-							value={formData.email}
-							onChange={handleChange}
-							/>
-						</label>
-						<br/>
-
-						<label>
-							Message:
-							<textarea
-							name="message"
-							value={formData.message}
-							onChange={handleChange}
-							/>
-						</label>
-						<br />
-						<button type="submit">Submit</button>
-					</form>
-				</div>
-			</div>
-
-			<Footer/>
-		</div>
-	);
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Contact;
