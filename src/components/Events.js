787d6d0ec2cc3e63fb4b3681f6b7b59ef7654f0b
infultMK2 <infernusultimo@gmail.com>
2024-07-02 04:29:17 -0400

    card layout created

--- src/components/Events.js
+++ src/components/Events.js
@@ -13,15 +13,27 @@ const Events = () => {
 	
 	return (
 		<div id='eventContainer'>
-			<h1 id='header'>Events</h1>
+			<h1 id='header'>Upcoming events</h1>
 			<div id="events">
 			{EVENTS.map(event => (
 					<div className='card'>
 						<div id='cardface'>
-							<span>{event.name}</span>
+							<p>{event.name}</p>
 						</div>
-						<div id='cardHeader' className='overlay'>Title: We do be like that doe.</div>
-						<div id='cardFooter' className='overlay'>lorem ipsum dolor sit amet.</div>
+						<div id='cardHeader' className='overlay'>
+							<label id='eventName'>{event.name}</label>
+						</div>
+						<div id='cardFooter' className='overlay'>
+							<div id='footDetails'>
+								<label id='eventDetails'>{event.description}</label>
+								<div id='venueGrid'>
+									<label id='a'>{event.venue}</label>
+									<label>{event.startTime}</label>
+									<label id='c'>{event.date}</label>
+								</div>
+								<button id='redirect'>View Details</button>
+							</div>
+						</div>
 					</div>
 			))}
 		</div>
