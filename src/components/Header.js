45628601135a413e0fc546d6a1e9369754d1a52f
infultMK2 <infernusultimo@gmail.com>
2024-10-15 01:23:50 -0400

    resized navbar and added socials with mobile changes too

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react
 import '../styles/Header.css';
 
 import roboticsLogo from "../assets/robotics-logo.png"
+import insta from '../assets/svg/instagram-white-icon.svg';
+import linkedin from '../assets/svg/linkedin-app-white-icon.svg';
+import github from '../assets/svg/github-mark-white.svg';
+import discord from '../assets/svg/discord-white-seeklogo-4.svg';
 
 const Header = () => {
 	const navigate = useNavigate();
@@ -101,12 +105,19 @@ const Header = () => {
 		<nav className={showNav ? 'headerContainer' : 'headerContainer hiddenNav'}>
 			<div id='logoContainer' onClick={handleHomeLinkClick}>
 				<img className='logo' src={roboticsLogo} alt="Robotics Logo"></img>
+				<span id='logoName'>UTM Robotics</span>
 			</div>
-				{/* <span id='logoName'>UTM Robotics</span> */}
 			<div id='pages'>
 				<button onClick={handleEventsLinkClick} id='redirect'>Events</button>
 				<button onClick={handleAboutLinkClick} id='redirect'>About</button>
 				<button onClick={handleContactLinkClick} id='redirect'>Contact</button>
+			</div>
+
+			<div className='socials'>
+				<a href='https://www.instagram.com/utm_robotics/' target='_blank'><img src ={insta}></img></a>
+				<a href='https://www.linkedin.com/company/utmrobotics/mycompany/' target='_blank'><img src = {linkedin}></img></a>
+				<a href='https://github.com/UTM-Robotics/' target='_blank'><img src= {github}></img></a>
+				<a href='https://discord.gg/wFbtZhcXFQ' target='_blank'><img src = {discord}></img></a>
 			</div>
 		</nav>
 	);
